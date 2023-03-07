2a354c7edbeb783b1caf95adde19a7c0a6e9c97f
Schmitt François <Schmitt.francois.1699@gmail.com>
2023-03-07 17:28:44 +0100

    API -- add UserLogin - with token Bearer

--- back-end/Server/controller/userController.js
+++ back-end/Server/controller/userController.js
@@ -27,15 +27,39 @@ exports.userCreate = (req, res) => {
 };
 
 
+exports.userLogin = (req, res, next) => {
+    Userdb.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                    }
+                    res.status(200).json({
+                        userId: user.id,
+                        token: jwt.sign(
+                            { userId: user.id },
+                            'RANDOM_TOKEN_SECRET',
+                            { expiresIn: '24h' }
+                        )
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+};
+
 exports.findUsers = (req, res, next) => {
-    Userdb.find()
-        .then(AllUsers => res.status(200).json(AllUsers))
-        .catch(error => res.status(400).json({ error }));
-}
+  Userdb.find()
+    .then((AllUsers) => res.status(200).json(AllUsers))
+    .catch((error) => res.status(400).json({ error }));
+};
 
 exports.findUserByID = (req, res, next) => {
-    Userdb.findOne({ _id: req.params.id })
-        .then(userId => res.status(200).json(userId))
-        .catch(error => res.status(404).json({ error }));
-}
-
+  Userdb.findOne({ _id: req.params.id })
+    .then((userId) => res.status(200).json(userId))
+    .catch((error) => res.status(404).json({ error }));
+};
--- back-end/Server/routes/router.js
+++ back-end/Server/routes/router.js
@@ -2,7 +2,8 @@ const express = require("express");
 const route = express.Router();
 const userController = require("../controller/userController");
 
-route.post("/api/users", userController.userCreate);
+route.post("/api/userCreate", userController.userCreate);
+route.post("/api/userLogin", userController.userLogin);
 route.get("/api/findUsers", userController.findUsers);
 route.get("/api/findUsers/:id", userController.findUserByID);
 
